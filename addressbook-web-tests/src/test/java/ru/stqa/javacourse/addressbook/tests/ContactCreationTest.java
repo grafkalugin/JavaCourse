95535fe7c8cc3acb3325c9061458b3a257c675cf
sunlab <aleksandr.kalugin@csssr.com>
2016-09-02 23:25:17 +0300

    задание 15

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/ContactCreationTest.java
@@ -1,22 +1,46 @@
 package ru.stqa.javacourse.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.javacourse.addressbook.model.ContactData;
 import ru.stqa.javacourse.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-	@Test
-    public void testContactCreation() {
+	@DataProvider
+	public Iterator<Object[]> validContactsFromJson() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json"))); // нужно менять расширение файла
+		String json = "";
+		String line = reader.readLine();
+		while (line != null){
+			json += line;
+			line = reader.readLine();
+		}
+		Gson gson = new Gson();
+		List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); // десериализация json // List<ContactData>.class
+		return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator(); // map к каждому объекту применяет функцию, которая заворачивает его в массив состоящий из этого объекта // collect из потока собирает список // и у списка берем итератор
+	}
+
+	@Test (dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contactdata) {
 		app.goTo().goHome();
 		Contacts before = app.contact().allContacts();
 	    app.goTo().goToContactCreation();
 		File photo = new File("src/test/resources/stru.png");
+		/*
 		ContactData contactdata = new ContactData()
 				.withFirstname("firstname")
 				.withLastname("lastname")
@@ -27,6 +51,7 @@ public class ContactCreationTest extends TestBase {
 				.withEmail("email@test.ru")
 				.withGroup("group name")
 				.withPhoto(photo);
+		*/
 		app.contact().createContact(contactdata);
 		assertThat(app.contact().count(), equalTo(before.size() +  1));
 		Contacts after = app.contact().allContacts();
