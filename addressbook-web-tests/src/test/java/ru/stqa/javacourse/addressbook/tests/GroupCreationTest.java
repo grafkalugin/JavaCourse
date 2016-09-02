21cbdc3682b6076c7cc716448d844abe1fbf1dc7
sunlab <aleksandr.kalugin@csssr.com>
2016-09-02 09:37:02 +0300

    lesson 6.5

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/GroupCreationTest.java
@@ -1,17 +1,39 @@
 package ru.stqa.javacourse.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.javacourse.addressbook.model.GroupData;
 import ru.stqa.javacourse.addressbook.model.Groups;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
-	    GroupData group = new GroupData().withName("group name");
+	@DataProvider
+	public Iterator<Object[]> validGroups() throws IOException {
+		List<Object[]> list = new ArrayList<Object[]>();
+		BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+		String line = reader.readLine();
+		while (line != null){
+			String[] split = line.split(";");
+			list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+			line = reader.readLine();
+		}
+		return list.iterator();
+	}
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
+	    //GroupData group = new GroupData().withName("group name");
 	    app.goTo().groupPage();
 	    Groups before = app.group().all();
 	    app.group().create(group);
